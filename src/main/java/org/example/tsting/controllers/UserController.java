d1a72fb4dcecb6a32e27de6061454cff32ce08e2
Pr1ntel <vandushev.sergey@yandex.ru>
2024-06-03 23:21:20 +0300

    add get users

--- src/main/java/org/example/tsting/controllers/UserController.java
+++ src/main/java/org/example/tsting/controllers/UserController.java
@@ -1,13 +1,27 @@
 package org.example.tsting.controllers;
 
 import lombok.AllArgsConstructor;
+import org.example.tsting.dtos.project_dto.ProjectResponseDto;
+import org.example.tsting.dtos.user_dto.UserResponseDto;
+import org.example.tsting.servises.UserService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @AllArgsConstructor
 @RequestMapping("/api/v1/main")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping(value = "/all-users")
+    public List<UserResponseDto> getAllUsers() {
+        return userService.getAll();
+    }
+
 }
--- src/main/java/org/example/tsting/servises/UserService.java
+++ src/main/java/org/example/tsting/servises/UserService.java
@@ -2,10 +2,30 @@ package org.example.tsting.servises;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.example.tsting.dtos.user_dto.UserResponseDto;
+import org.example.tsting.repositorys.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @AllArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    public List<UserResponseDto> getAll() {
+
+
+        return userRepository.findAll().stream().map(
+                users -> UserResponseDto.builder()
+                        .id(users.getId())
+                        .firstName(users.getFirstName())
+                        .lastName(users.getLastName())
+                        .login(users.getLogin())
+                        .roleName(users.getRoleName().getRoleName())
+                        .build()
+        ).collect(Collectors.toList());
+    }
+
 }
